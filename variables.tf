340ebcffd54edb556efdcbcd716982ea339bb55a
Első András <elso.andras@gmail.com>
2025-07-29 16:14:02 +0200

    Allow proxmox username auth

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "proxmox" {
     insecure           = optional(bool)
     username           = optional(string)
     password           = optional(string)
+    realm              = optional(string, "pam")
     api_token          = optional(string)
     ssh_agent          = optional(string, false)
     random_vm_ids      = optional(string, false)

ef761bb622987d4d48c81bdf69a237ec9ecbefb3
Első András <elso.andras@gmail.com>
2025-07-29 22:20:35 +0200

    Add cluster ip

--- modules/talos_k8s/main.tf
+++ modules/talos_k8s/main.tf
@@ -1,19 +1,20 @@
 locals {
   first_control_plane = [for k, v in var.nodes : v.ip if v.machine_type == "controlplane"][0]
+  cluster_vip         = var.cluster.endpoint != null ? "https://${var.cluster.endpoint}:6443" : "https://${local.first_control_plane}:6443"
 }
 
 resource "talos_machine_secrets" "this" {}
 
 data "talos_machine_configuration" "controlplane" {
   cluster_name     = var.cluster.name
-  cluster_endpoint = var.cluster.network_dhcp ? "https://${local.first_control_plane}:6443" : "https://${var.cluster.endpoint}:6443"
+  cluster_endpoint = local.cluster_vip
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
 
 data "talos_machine_configuration" "worker" {
   cluster_name     = var.cluster.name
-  cluster_endpoint = var.cluster.network_dhcp ? "https://${local.first_control_plane}:6443" : "https://${var.cluster.endpoint}:6443"
+  cluster_endpoint = local.cluster_vip
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
@@ -32,10 +33,12 @@ resource "talos_machine_configuration_apply" "controlplane" {
     for k, v in var.nodes : k => v
     if v.machine_type == "controlplane"
   }
-  node = each.value.ip
+  node     = each.value.ip
+  endpoint = each.value.ip
   config_patches = [
     templatefile("${path.module}/config/control-plane.yaml.tmpl", {
       hostname       = "${var.cluster.name}-${each.key}"
+      cluster_vip    = var.cluster.endpoint
       install_disk   = each.value.install_disk
       cilium_values  = file("${path.module}/kubernetes/cilium-values.yaml")
       cilium_install = file("${path.module}/kubernetes/cilium-install.yaml")

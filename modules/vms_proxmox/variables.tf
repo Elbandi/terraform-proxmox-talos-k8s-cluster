114744e3a780c26a55fc89e672ed562b2e72d070
Első András <elso.andras@gmail.com>
2025-07-30 11:26:31 +0200

    Allow set iso image datastore

--- modules/vms_proxmox/variables.tf
+++ modules/vms_proxmox/variables.tf
@@ -11,6 +11,7 @@ variable "proxmox" {
     random_vm_ids      = optional(string, false)
     random_vm_id_start = optional(number, 1000)
     random_vm_id_end   = optional(number, 2000)
+    iso_datastore_id   = optional(string, "local")
   })
   sensitive = true
 }

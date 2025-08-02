3a62a4f112df928c133ed79f86e304342dcf1ff1
Első András <elso.andras@gmail.com>
2025-08-02 12:13:39 +0200

    Add more options to argocd

--- variables.tf
+++ variables.tf
@@ -95,6 +95,17 @@ variable "argocd" {
     admin_password = string
     namespace      = string
     chart_version  = string
+    oidc_config = optional(object({
+      name             = string
+      issuer           = string
+      client_id        = string
+      client_secret    = string
+      requested_scopes = list(string)
+    }))
+    custom_rbac = optional(object({
+      scopes = list(string)
+      policy = list(string)
+    }))
   })
   default = null
 }
@@ -103,20 +114,22 @@ variable "repo" {
   description = "Git repo and path to the ArgoCD Applications"
 
   type = object({
-    name          = string
-    repo_url      = string
-    branch        = optional(string, "main")
-    manifest_path = string
-    project_name  = optional(string, "default")
-    recurse       = optional(bool, true)
+    name            = string
+    repo_url        = string
+    branch          = optional(string, "main")
+    manifest_path   = string
+    project_name    = optional(string, "default")
+    recurse         = optional(bool, true)
+    ssh_known_hosts = optional(list(string))
   })
   default = null
 }
 
 variable "git_credentials" {
   type = object({
-    username = string
-    password = string
+    username    = string
+    password    = optional(string)
+    private_key = optional(string)
   })
   description = "Git repository credentials"
   default     = null

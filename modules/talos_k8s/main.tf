424325fb958d61ffdade34452d4f9440658c07db
Első András <elso.andras@gmail.com>
2025-11-13 13:37:34 +0100

    Move calico manitest to files

--- modules/talos_k8s/main.tf
+++ modules/talos_k8s/main.tf
@@ -49,6 +49,8 @@ resource "talos_machine_configuration_apply" "controlplane" {
         cluster_id   = var.cluster.id
       })
       cilium_install = file("${path.module}/kubernetes/cilium-install.yaml")
+      calico_felix   = file("${path.module}/kubernetes/calico-felix.yaml")
+      calico_install = file("${path.module}/kubernetes/calico-install.yaml")
       enable_lvm     = anytrue(flatten([for i, n in var.nodes : [for j, d in n.data_disks : d.type == "lvm"]]))
       lvm_setup = templatefile("${path.module}/kubernetes/lvm-setup.yaml", {
         lvm_label_node = true
